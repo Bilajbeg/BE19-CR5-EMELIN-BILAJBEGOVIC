c89db3391b81173664e2be00ce9d83bb7fb68ffd
Emelin Bilajbegovic <bilajbeg@gmail.com>
2023-07-29 20:02:32 +0200

    new avatar pic for animals

--- upload.php
+++ upload.php
@@ -2,7 +2,7 @@
 function upload_user($picture)
 {
     if ($picture["error"] == 4) {
-        $pictureName = "avatar.jpg";
+        $pictureName = "avatar.png";
         $message = "You didn't choose any picture, but you can upload it later";
     } else {
         $checkIfImage = getimagesize($picture["tmp_name"]);
@@ -35,7 +35,7 @@ function upload($image)
         $destination = "pictures/{$pictureName}";
         move_uploaded_file($image["tmp_name"], $destination);
     } elseif ($message == "Not an image") {
-        $pictureName = "avatar.png";
+        $pictureName = "avatar.jpg";
         $message = "The file that you selected is not an image, you can upload it later";
     }
     return [$pictureName, $message];
